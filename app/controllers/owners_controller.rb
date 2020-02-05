94575474258ae8b467d3dd90a970df8e9c07822c
Jeff Swisher <jefftswisher@gmail.com>
2020-02-05 03:54:09 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -23,12 +23,18 @@ class OwnersController < ApplicationController
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  patch '/owners/:id' do
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    #######
+ 
     @owner = Owner.find(params[:id])
-    erb :'/owners/show'
-  end
-
-  patch '/owners/:id' do 
-   
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"  
   end
 end
